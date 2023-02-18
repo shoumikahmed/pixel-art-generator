d7c38798a4bcd0f55f497279f93943a6099cf30d
shoumikahmed <shoumikahmed103@gmail.com>
2023-02-18 23:16:11 +0600

    new

--- index.js
+++ index.js
@@ -8,3 +8,33 @@ let eraseBtn = document.getElementById("erase-btn");
 let paintBtn = document.getElementById("paint-btn");
 let widthValue = document.getElementById("width-value");
 let heightValue = document.getElementById("height-value");
+
+let events = {
+    mouse: {
+        down: "mousedown",
+        move: "mousemove",
+        up: "mouseup"
+    },
+    touch: {
+        down: "touchstart",
+        move: "touchmove",
+        up: "touchend"
+    },
+}
+
+let deviceType = "";
+
+let draw = false;
+let erase = false;
+
+const isTouchDevice = () => {
+    try {
+        document.createEvent("TouchEvent");
+        deviceType = "touch";
+        return true;
+    }
+    catch (e) {
+        deviceType = "mouse";
+        return false
+    }
+}
